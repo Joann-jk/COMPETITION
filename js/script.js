101ac0de47062f3a1d140493bfa5a560d6eb340f
Joann <joannjkoodathil@gmail.com>
2024-09-21 16:24:29 +0530

    oky

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@ const courses = [
         description: "Learning HTML for developing modern interactive   web applications.",
         rating: 4.5,
         tags: ["Engineering", "Testing"],
-        image: "../images/Card Image.png"
+        image: "images/Card Image.png"
     },
     {
         title: "CSS for mobile",
         description: "You'll learn about CSS, layouts, wrappers, creating a sticky navbar, and more.",
         rating: 4.3,
         tags: ["Engineering", "Testing"],
-         image: "../images/Card Image (1).png"
+         image: "images/Card Image (1).png"
     },
     {
         title: "JavaScript",
         description: "This course provides a comprehensive introduction to the JavaScript programming language.",
         rating: 4.7,
         tags: ["JavaScript", "Development"],
-         image: "../images/Card Image (2).png"
+         image: "images/Card Image (2).png"
     }
 ];
 
@@ -88,10 +88,10 @@ function renderCards() {
 document.addEventListener('DOMContentLoaded', renderCards);
 
 const mentors = [
-    { name: "John Wilson", role: "UI / UX Designer", experience: "6 yr.", image: "../images/Avatar.png" },
-    { name: "Daniel Hill", role: "Test Engineer", experience: "5 yr.", image: "../images/Avatar (1).png" },
-    { name: "Sofia Harris", role: "Python Developer", experience: "8 yr.", image: "../images/Avatar (2).png" },
-    { name: "Eva Smith", role: "Motion Designer", experience: "4 yr.", image: "../images/Avatar (3).png" }
+    { name: "John Wilson", role: "UI / UX Designer", experience: "6 yr.", image: "images/Avatar.png" },
+    { name: "Daniel Hill", role: "Test Engineer", experience: "5 yr.", image: "images/Avatar (1).png" },
+    { name: "Sofia Harris", role: "Python Developer", experience: "8 yr.", image: "images/Avatar (2).png" },
+    { name: "Eva Smith", role: "Motion Designer", experience: "4 yr.", image: "images/Avatar (3).png" }
 ];
 
 function createMentorCard(mentor) {
